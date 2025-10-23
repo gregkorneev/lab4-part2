2359cd5b5b4eea4b09be59fcfb4e1e9500a00089
gregkorneev <gregory.korneev@gmail.com>
2025-10-23 12:58:41 +0300

    lab4-part2

---
+++ build/CMakeFiles/Lab4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4.dir/link.d"
+  "CMakeFiles/Lab4.dir/src/main.cpp.o"
+  "CMakeFiles/Lab4.dir/src/main.cpp.o.d"
+  "Lab4"
+  "Lab4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
